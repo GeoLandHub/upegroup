5a2308809d6963c318bc92af3537a45c2bfc6678
GeoLandHub <upegroup.04@gmail.com>
2025-08-16 19:11:36 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './GeoLandHubMockup.jsx' // หรือไฟล์หลักของคุณ
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./GeoLandHubMockup.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
