758a0a8d59b2a6ae708242ac3a3f215feb3beb06
GeoLandHub <upegroup.04@gmail.com>
2025-08-16 16:02:57 +0700

    Delete src/src directory

--- src/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import GeoLandHubMockup from "./GeoLandHubMockup.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <GeoLandHubMockup />
-  </React.StrictMode>
-);
